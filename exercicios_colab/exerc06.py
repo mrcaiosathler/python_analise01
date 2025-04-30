14f6d9f877d03ae65933a8db72487a5e5e192e72
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-30 19:15:21 -0300

    ex06 - colab

---
+++ exercicios_colab/exerc06.py
@@ -0,0 +1,9 @@
+## Exercício 6: Verifique se um usuário pode votar (idade >= 18).
+
+idade = int(input('Informe sua idade, por favor: '))
+if idade >= 18:
+  print('O usuário já pode votar!')
+else:
+  print ('O usuário ainda não tem idade para votar!')
+
+  
