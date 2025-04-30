1a98679444e12de54a1776f0883c89455351169f
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-30 19:47:28 -0300

    ex18 - colab

---
+++ exerc18 .py
@@ -0,0 +1,7 @@
+## Exercício 18: Retorne 'Adulto' se a idade for maior ou igual a 18, senão 'Menor de idade'.
+
+idade = int(input('Informe sua idade: '))
+if idade >=18:
+  print('Adulto')
+else:
+  print('Menor de idade')
