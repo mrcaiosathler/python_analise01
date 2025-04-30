f6c2e6cf3fedccfaad0a3ecca69f50bfaaf73d58
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-30 19:49:57 -0300

    ex20 - colab

---
+++ exerc20.py
@@ -0,0 +1,7 @@
+## Exercício 20: Retorne 'Aprovado' se a nota for maior ou igual a 7, senão 'Reprovado'.
+
+nota = float(input('Informe sua nota, por favor:'))
+if nota >=7:
+  print('Aprovado!')
+else:
+  print('Reprovado!')
