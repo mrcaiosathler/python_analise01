0ac23c8fb20f581625107b3232f834b1b849c0c6
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-30 19:48:17 -0300

    ex19 - colab

---
+++ exerc19.py
@@ -0,0 +1,7 @@
+## Exercício 19: Verifique se um número é maior que 100 e retorne 'Grande' ou 'Pequeno'.
+
+num = float(input('Informe um número, por favor:'))
+if num > 100:
+  print('Grande!')
+else:
+  print('Pequeno!')
