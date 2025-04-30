2aee0eed15abc3cf7cebac4d85fceadcbfb53904
Caio Sathler de Lima <sathler.caio@gmail.com>
2025-04-30 19:25:38 -0300

    ex09 - colab

---
+++ exercicios colab/exerc09.py
@@ -0,0 +1,7 @@
+## Exercício 9: Verifique se um número está entre 10 e 50.
+
+num = float(input('Informe um número: '))
+if  10 < num < 50:
+  print('O número está entre 10 e 50')
+else:
+  print('O número não está entre 10 e 50')
